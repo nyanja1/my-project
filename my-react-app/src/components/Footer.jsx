21450a7062a48698ab115d6feff940120b985281
nyanja1 <ogollanyanja@gmail.com>
2025-05-05 11:50:31 +0300

    added tailwind csss

---
+++ my-react-app/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <footer className="bg-gray-800 p-4 text-center">
+        <p className="text-gray-400">© 2025 Elvis Amonde. All rights reserved.</p>
+      </footer>
+    );
+  }
